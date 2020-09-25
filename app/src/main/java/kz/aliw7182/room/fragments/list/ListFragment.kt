f684ea47ca73a5c7cae5b3e74be3979322415bd3
aliw7182 <zhanbulataliser@gmail.com>
2020-09-25 14:53:40 +0600

    added ability to read data from db

--- app/src/main/java/kz/aliw7182/room/fragments/list/ListFragment.kt
+++ app/src/main/java/kz/aliw7182/room/fragments/list/ListFragment.kt
@@ -5,11 +5,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Adapter
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.fragment_list.*
 import kotlinx.android.synthetic.main.fragment_list.view.*
 import kz.aliw7182.room.R
+import kz.aliw7182.room.data.UserViewModel
 
 class ListFragment : Fragment() {
+    private lateinit var mUserViewModel: UserViewModel
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 
@@ -17,6 +25,11 @@ class ListFragment : Fragment() {
         view.floatingActionButton.setOnClickListener {
             findNavController().navigate(R.id.action_listFragment_to_addFragment)
         }
+        val adapter = UserAdapter()
+        view.recyclerView.adapter = adapter
+        view.recyclerView.layoutManager = LinearLayoutManager(requireContext())
+        mUserViewModel = ViewModelProvider(this).get(UserViewModel::class.java)
+        mUserViewModel.readAllData.observe(viewLifecycleOwner, Observer(adapter::setData))
         return view
     }
 
---
+++ app/src/main/java/kz/aliw7182/room/fragments/list/UserAdapter.kt
@@ -0,0 +1,40 @@
+package kz.aliw7182.room.fragments.list
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.item_user.view.*
+import kz.aliw7182.room.R
+import kz.aliw7182.room.data.User
+
+class UserAdapter: RecyclerView.Adapter<UserViewHolder>() {
+    private var userList = emptyList<User>()
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserViewHolder {
+return UserViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_user,parent,false))
+    }
+
+
+    override fun onBindViewHolder(holder: UserViewHolder, position: Int) {
+        val currentItem = userList[position]
+        holder.itemView.item_id.text = currentItem.id.toString()
+        holder.itemView.item_name.text = currentItem.firstName
+        holder.itemView.item_lastname.text = currentItem.lastName
+        holder.itemView.item_age.text = currentItem.age.toString()
+    }
+
+
+    override fun getItemCount(): Int {
+        return userList.size
+    }
+    fun setData(user:List<User>){
+        this.userList = user
+        notifyDataSetChanged()
+    }
+
+}
+
+class UserViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+
+}
